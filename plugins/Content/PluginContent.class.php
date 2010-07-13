1c700ffa110135d7cd19f6b2d7c06797a9f5819c
haikataker <haikataker@54b7c828-e0ec-465f-b0b8-848fba75e129>
2010-07-13 16:36:47 +0000

    (Branch:ClassDesign) Added file prerequisits. Added fields for the plugin's database table names. Added dummies for all functions that need to be implemented (abstracts in the parent class). Added overriding constructor which determines the plugin's database table names.
    git-svn-id: https://indiss.svn.sourceforge.net/svnroot/indiss/branches/v0.2-a_ClassDesign@154 54b7c828-e0ec-465f-b0b8-848fba75e129

--- plugins/Content/PluginContent.class.php
+++ plugins/Content/PluginContent.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2010-06-23
+ * @version     2010-07-13
  * @author      Patrick Lehner <lehner.patrick@gmx.de>
  * @copyright   Copyright (C) 2010 Patrick Lehner
  * @module      
@@ -19,9 +19,13 @@
  *              along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+defined("__MAIN") or die("Restricted access.");
+defined("__CONFIGFILE") or die("Config file not included [" . __FILE__ . "]");
+class_exists("Plugin") or die("Class 'Plugin' is unknown [" . __FILE__ . "]");
+
 /**
  * 
- * @author Patrick
+ * @author Patrick Lehner
  * 
  */
 class PluginContent extends Plugin {
@@ -31,14 +35,69 @@ class PluginContent extends Plugin {
     
     //---- Object properties ------------------------------------------------------------
     
+    private $itemTable;
+    private $optionTable;
+    
     
     //---- Static methods ---------------------------------------------------------------
     
     
     //---- Constructors & destructors ---------------------------------------------------
     
+    public function __construct($pluginInstanceInfo, $pluginInfo) {
+        parent::__contstruct($pluginInstanceInfo, $pluginInfo);
+        $this->itemTable = $this->iname . "_items";
+        $this->optionTable = $this->iname . "_options";
+    }
+    
     
     //---- Object methods ---------------------------------------------------------------
+    
+    public function hasFrontend() {
+        return true;
+    }
+    
+    public function initialize() {
+        
+    }
+    
+    public function install() {
+        global $log, $db;
+        
+        //create database tables
+        $query = "CREATE TABLE IF NOT EXISTS `com_content_1` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `name` varchar(127) COLLATE utf8_unicode_ci NOT NULL,
+                `url` varchar(1023) COLLATE utf8_unicode_ci NOT NULL,
+                `displaytime` int(11) NOT NULL,
+                `start` datetime NOT NULL,
+                `end` datetime NOT NULL,
+                `type` enum('LocalPage','ExternalPage','LocalImage','ExternalImage','LocalPDF','ExternalPDF','LocalFlash','ExternalFlash','LocalOther','ExternalOther','Plugin','Unknown') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Unknown',
+                `enabled` tinyint(1) NOT NULL,
+                `deleted` tinyint(1) NOT NULL,
+                `tags` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+                `createdBy` int(11) NOT NULL DEFAULT '0',
+                `createdAt` datetime NOT NULL,
+                `modifiedBy` int(11) NOT NULL DEFAULT '0',
+                `modifiedAt` datetime NOT NULL,
+                PRIMARY KEY (`id`)
+            ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;";
+        if (!$db->q()) {
+            
+        }
+    }
+    
+    public function uninstall() {
+        
+    }
+    
+    public function outputFront() {
+        
+    }
+    
+    public function outputAdmin() {
+        
+    }
     
 
 }
