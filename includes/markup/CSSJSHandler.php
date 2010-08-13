b5f9226d6a3b2dc5af2c51f983d514a3c83dc502
haikataker <haikataker@54b7c828-e0ec-465f-b0b8-848fba75e129>
2010-08-13 14:42:27 +0000

    (Branch:ClassDesign) Extended class to actually do something. Can now take up styles, scripts, or respective links and output them appropriately.
    git-svn-id: https://indiss.svn.sourceforge.net/svnroot/indiss/branches/v0.2-a_ClassDesign@207 54b7c828-e0ec-465f-b0b8-848fba75e129

--- includes/markup/CSSJSHandler.php
+++ includes/markup/CSSJSHandler.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2010-07-13
+ * @version     2010-08-13
  * @author      Patrick Lehner <lehner.patrick@gmx.de>
  * @copyright   Copyright (C) 2010 Patrick Lehner
  * @module      
@@ -26,26 +26,102 @@ class CSSJSHandler {
 
     //---- Static properties ------------------------------------------------------------
     
+    private static
+        $scripts = array(),
+        $scriptUrls = array(),
+        $styles = array(),
+        $styleUrls = array();
+    
     
     //---- Object properties ------------------------------------------------------------
     
     
     //---- Static methods ---------------------------------------------------------------
     
-    public static function addStyle() {
-        
+    public static function addStyle($style) {
+        self::$styles[] = $style;
     }
     
-    public static function addStyleUrl() {
-        
+    public static function addStyleUrl($url) {
+        self::$styleUrls[] = $url;
     }
     
-    public static function addScript() {
-        
+    public static function addScript($script) {
+        self::$scripts[] = $script;
     }
     
-    public static function addScriptUrl() {
+    public static function addScriptUrl($url) {
+        self::$scriptUrls = $url;
+    }
+    
+    private static function outputStyles(&$indentStr) {
+        $s = "";
         
+        if (count(self::$styles) > 0) {
+            foreach (self::$styles as $style) {
+                $s .= $indentStr . "<style type=\"text/css\">\n";
+                $s .= $style . "\n";
+                $s .= $indentStr . "</style>\n";
+            }
+            $s .= "\n";
+        }
+        
+        return $s;
+    }
+    
+    private static function outputStyleUrls(&$indentStr) {
+        $s = "";
+        
+        if (count(self::$styleUrls) > 0) {
+            foreach (self::$styleUrls as $url) {
+                $s .= $indentStr . "<link rel=\"stylesheet\" type=\"text/css\" href=\"$url\" />\n";
+            }
+            $s .= "\n";
+        }
+        
+        return $s;
+    }
+    
+    private static function outputScripts(&$indentStr) {
+        $s = "";
+        
+        if (count(self::$scripts) > 0) {
+            foreach (self::$scripts as $script) {
+                $s .= $indentStr . "<script type=\"text/javascript\">\n";
+                $s .= $script . "\n";
+                $s .= $indentStr . "</script>\n";
+            }
+            $s .= "\n";
+        }
+        
+        return $s;
+    }
+    
+    private static function outputScriptUrls(&$indentStr) {
+        $s = "";
+        
+        if (count(self::$scriptUrls) > 0) {
+            foreach (self::$scriptUrls as $url) {
+                $s .= $indentStr . "<link rel=\"stylesheet\" type=\"text/css\" href=\"$url\" />\n";
+            }
+            $s .= "\n";
+        }
+        
+        return $s;
+    }
+    
+    public static function outputAll($indent = 0) {
+        $s = "";
+        for ($i = 0; $i < $indent; $i++)
+            $s .= " ";
+        
+        $r = "";
+        $r .= self::outputStyleUrls($s);
+        $r .= self::outputScriptUrls($s);
+        $r .= self::outputStyles($s);
+        $r .= self::outputScripts($s);
+        
+        return $r;
     }
     
     
