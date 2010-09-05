52c078bd8afa292d2a868be48d6446e6b44075b1
haikataker <haikataker@54b7c828-e0ec-465f-b0b8-848fba75e129>
2010-09-05 09:57:07 +0000

    (Branch:ClassDesign) Moved ZIP file handling below branching for upload/download, added validity check and call to PluginMan::install(). Added recursive deletion of unpacked temp files, which is however commented out at the moment for debugging.
    git-svn-id: https://indiss.svn.sourceforge.net/svnroot/indiss/branches/v0.2-a_ClassDesign@229 54b7c828-e0ec-465f-b0b8-848fba75e129

--- plugins/PluginManager/postviews/addNew.php
+++ plugins/PluginManager/postviews/addNew.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2010-09-04
+ * @version     2010-09-05
  * @author      Patrick Lehner <lehner.patrick@gmx.de>
  * @copyright   Copyright (C) 2010 Patrick Lehner
  * @module      
@@ -28,32 +28,73 @@ global $FBP;
 
 if ($_POST["sourcetype"] == "upload") {
     if (is_uploaded_file($_FILES["pluginFile"]["tmp_name"])) {
-        
-        $z = new ZipArchive();
-        $z->open($_FILES["pluginFile"]["tmp_name"]);
-        
-        /*print_r($z);
-        var_dump($z);
-        echo "numFiles: " . $z->numFiles . "\n";
-        echo "status: " . $z->status  . "\n";
-        echo "statusSys: " . $z->statusSys . "\n";
-        echo "filename: " . $z->filename . "\n";
-        echo "comment: " . $z->comment . "\n";
-        
-        for ($i=0; $i<$z->numFiles;$i++) {
-            echo "index: $i\n";
-            print_r($z->statIndex($i));
-        }*/
-        
-        $path = str_replace("\\","/",$FBP . "temp");
-        if ($z->extractTo($path))
-            echo "ok";
-            else echo "error";
-        
-        $z->close();
+        $zipfile = $_FILES["pluginFile"]["tmp_name"];
+        $destfolder = $FBP . "temp/" . basename($_FILES["pluginFile"]["tmp_name"]);
+    } else {
+        //Attention: need to observe behaviour of this statement if there is code after the inclusion call in PluginPluginManager!
+        return;
     }
 } else if ($_POST["sourcetype"] == "download") {
     
 }
+
+//open ZIP file
+$z = new ZipArchive();
+$z->open($zipfile);
+
+/*//output debug info
+print_r($z);
+var_dump($z);
+echo "numFiles: " . $z->numFiles . "\n";
+echo "status: " . $z->status  . "\n";
+echo "statusSys: " . $z->statusSys . "\n";
+echo "filename: " . $z->filename . "\n";
+echo "comment: " . $z->comment . "\n";
+
+for ($i=0; $i<$z->numFiles;$i++) {
+    echo "index: $i\n";
+    print_r($z->statIndex($i));
+}*/
+
+//check if ZIP file is a valid plugin archive
+$valid = ($z->numFiles > 0);
+if ($valid) { //the first entry in the archive must be a folder
+    $item = $z->statIndex(0);
+    $valid = (substr($item["name"], -1) == "/") && ($item["size"] == 0);
+    $foldername = substr($item["name"], 0, -1);
+}
+if ($valid) { //and within that top-level folder must be a pluginInfo.xml file
+    $item = $z->statName($foldername . "/pluginInfo.xml");
+    $valid = ($z !== false);
+}
+
+$error = false;
+
+if ($valid) {
+    $destfolder = str_replace("\\","/", $destfolder); //replace any backslashes from Windows OS with slashes, or extractTo() will act up
+    if ($z->extractTo($destfolder)) {
+        
+    } else {
+        //output msg that extraction failed
+        echo "extract error";
+        $error = true;
+    }
+} else { //not $valid
+    //output msg that archive is not valid
+    $error = true;
+    echo "not valid";
+}
+
+//we are done with the ZIP file. close it to free its resources
+$z->close();
+
+if (!$error) {
+    //the rest of the installation is now done by PluginMan:
+    PluginMan::install($destfolder . "/" . $foldername);
+}
+
+include_once($FBP . "includes/filesystem/recursiveDelete.php");
+echo "recursive delete folder";
+//recursiveDelete($path . "/" . $foldername);
 
 ?>
